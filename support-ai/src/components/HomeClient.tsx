d4776a3ec7fd2c683ce409eee5631423c56dfa19
tejth <ptejendra91@gmail.com>
2026-02-17 12:41:39 +0530

    logout api setup

--- support-ai/src/components/HomeClient.tsx
+++ support-ai/src/components/HomeClient.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import { title } from "process";
+import axios from "axios";
 
 function HomeClient({ email }: { email: string }) {
   const handleLogin = () => {
@@ -29,18 +30,27 @@ function HomeClient({ email }: { email: string }) {
 
   const features = [
     {
-      title:"Plug & Play",
-      desc: "Add the cahtbot to your site with a single script tag."
+      title: "Plug & Play",
+      desc: "Add the cahtbot to your site with a single script tag.",
     },
     {
       title: "Admin Controlled",
-      desc: "You control exactly what the AI knows and answers."
+      desc: "You control exactly what the AI knows and answers.",
     },
     {
       title: "Always Online",
-      desc: "Your customers get instant support 24/7."
+      desc: "Your customers get instant support 24/7.",
+    },
+  ];
+
+  const handleLogOut = async () => {
+    try {
+      const result = await axios.get("/api/auth/logout");
+       window.location.href = "/";
+    } catch (error) {
+      console.log(error);
     }
-  ]
+  };
 
   return (
     <div className="min-h-screen bg-linear-to-br from-white to-zinc-50 text-zinc-900 overflow-x-hidden">
@@ -80,7 +90,10 @@ function HomeClient({ email }: { email: string }) {
                     <button className="w-full text-left px-4 py-3 text-sm hover:bg-zinc-100">
                       Dashboard
                     </button>
-                    <button className="block px-4 py-3 text-sm text-red-600 hover:bg-zinc-100">
+                    <button
+                      onClick={handleLogOut}
+                      className="block px-4 py-3 text-sm text-red-600 hover:bg-zinc-100"
+                    >
                       Logout
                     </button>
                   </motion.div>
@@ -99,9 +112,7 @@ function HomeClient({ email }: { email: string }) {
       </motion.div>
 
       <section className="pt-36 pb-28 px-6">
-
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-20 items-center">
-         
           <motion.div
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
@@ -136,111 +147,89 @@ function HomeClient({ email }: { email: string }) {
                 </button>
               )}
 
-              <a href="#feature" className="px-7 py-3 rounded-xl border border-zinc-700 hover:bg-zinc-100 transition">
+              <a
+                href="#feature"
+                className="px-7 py-3 rounded-xl border border-zinc-700 hover:bg-zinc-100 transition"
+              >
                 Learn More
               </a>
             </div>
           </motion.div>
-         
-
 
           <motion.div
-            initial={{opacity:0 , scale: 0.95}}
-            animate={{opacity:1 , scale:1}}
-            transition={{ duration:0.7 , delay:0.2}}
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.7, delay: 0.2 }}
             className="relative"
           >
-
             <div className="rounded-2xl bg-white shadow-2xl border border-zinc-200 p-6">
-                <div className="text-sm text-zinc-500 mb-3">
-                  Live Chat Preview
+              <div className="text-sm text-zinc-500 mb-3">
+                Live Chat Preview
+              </div>
+              <div className="space-y-3">
+                <div className="bg-black text-white rounded-lg px-4 py-2 text-sm ml-auto w-fit ">
+                  Do you offer cash on delivery?
                 </div>
-                <div className="space-y-3">
-                  <div className="bg-black text-white rounded-lg px-4 py-2 text-sm ml-auto w-fit ">Do you offer cash on delivery?</div>
-                  <div className="bg-zinc-100 rounded-lg px-4 py-2 text-sm w-fit">Yes, Cash on Delivery is available!</div>
-                  
+                <div className="bg-zinc-100 rounded-lg px-4 py-2 text-sm w-fit">
+                  Yes, Cash on Delivery is available!
                 </div>
-                <motion.div
-                 animate={{y:[0,-12,0]}}
-                 transition={{repeat:Infinity , duration:3}}
-                 className="
+              </div>
+              <motion.div
+                animate={{ y: [0, -12, 0] }}
+                transition={{ repeat: Infinity, duration: 3 }}
+                className="
                  absolute -bottom-6 -right-6
                  w-14 h-14 rounded-full
                  bg-black text-white
                  flex items-center justify-center
                  shadow-xl
                  "
-                
-                >
-                   💭
-                </motion.div>
-                                           
+              >
+                💭
+              </motion.div>
             </div>
-
-          
-
-          </motion.div> 
-
-
+          </motion.div>
         </div>
-
-
-      
-
-
-
-      
-      
       </section>
 
-
       <section
-       id="feature"
-       className="bg-zinc-50 py-28 px-6 border-t border-zinc-200"
+        id="feature"
+        className="bg-zinc-50 py-28 px-6 border-t border-zinc-200"
       >
-
         <div className="max-w-6xl mx-auto">
           <motion.h2
-          initial={{opacity:0 , y:20}}
-          whileInView={{opacity:1 , y:0}}
-          viewport={{once:false}}
-          transition={{duration:0.5}}
-          className="text-3xl font-semibold text-center"
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: false }}
+            transition={{ duration: 0.5 }}
+            className="text-3xl font-semibold text-center"
           >
             Why Businesses Choose SupportAI
           </motion.h2>
 
-
           <div className="mt-16 grid grid-cols-1 md:grid-cols-3 gap-10">
-            {features.map((f,index)=>(
+            {features.map((f, index) => (
               <motion.div
-              key={index}
-              initial={{opacity:0 , y:30}}
-              whileInView={{opacity:1 , y:0}}
-              transition={{delay: index * 0.1}}
-              viewport={{once:false}}
-              className="
+                key={index}
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: index * 0.1 }}
+                viewport={{ once: false }}
+                className="
               bg-white rounded-2xl p-8 shadow-lg border border-zinc-200
               "
               >
-               <h1 className="text-lg font-medium">{f.title}</h1>
-              <p className="mt-3 text-zinc-600 text-sm">{f.desc}</p>
-
+                <h1 className="text-lg font-medium">{f.title}</h1>
+                <p className="mt-3 text-zinc-600 text-sm">{f.desc}</p>
               </motion.div>
             ))}
           </div>
-
         </div>
-
-        
       </section>
-
 
       <footer className="py-10 text-center text-sm text-zinc-500">
         &copy; {new Date().getFullYear()} SupportAI. All rights reserved.
       </footer>
-
-
     </div>
   );
 }
